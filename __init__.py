5af30044cbd00df27ad4a96e1327010dcc4021a6
Sean M. Gonzalez <sean.moore.gonzalez@gmail.com>
2016-01-14 15:18:30 -0500

    cleaning things up

--- __init__.py
+++ __init__.py
@@ -3,6 +3,6 @@
 # script by dedan: helps him to symlink gensim
 import os
 dirname = __path__[0]		# Package's main folder
-__path__.insert(0, os.path.join(dirname, "Synapsify"))
+__path__.insert(0, os.path.join(dirname, "Munge"))
 
 __all__ = ['loadCleanly']
